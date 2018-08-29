5bacad8030a5abcb1f311630733773e996326e52
Rsabaric <rosie.sabaric@gmail.com>
2018-08-29 03:37:41 +0000

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
 /* WATS 3020 Browser Game project */
+/* Rosie Sabaric */
 /* Build a tic tac toe game for two players. */
 
 // TODO: Create a class called `Player`. The `constructor()` should look for a
 // parameter called `token` and should set `this.token` as a property of
 // the class.
+class Player {
+    constructor(token){
+        this.token = token;
+    }
+}
 
 
 // Tic Tac Toe Game Class
 class TicTacToe {
     constructor(){
-        // TODO: Set up `this.player1` and `this.player2` properties.
+        // Set up `this.player1` and `this.player2` properties.
         // These properties should be new Player class instances.
         // You may set the "token" to anything that corresponds to a Glyphicon
         // icon name ('heart', 'star', 'remove-sign', 'unchecked', 'bell',
         // 'certificate', etc.)
+        this.player1 = new Player('remove-sign');
+        this.player2 = new Player('unchecked');
 
 
         // TODO: Initialize several  properties that will be used to track game
         // progress.
 
         // TODO: Set `this.currentPlayer` equal to `null`
+        this.currentPlayer = null;
 
         // TODO: Set `this.gameStatus` equal to `null`
-
+        this.gameStatus = null;
         // TODO: Set `this.winner` equal to `null`
+        this.winner = null;
 
         // TODO: Set `this.moveCount` equal to `0`
+        this.moveCount = O;
 
         // TODO: Set up DOM elements used in game as Class properties
 
